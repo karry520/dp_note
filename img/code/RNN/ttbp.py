69e61ca858bd9c395d88ec13659c14e535ca8d25
karry520 <884816926@qq.com>
2020-06-02 19:20:54 +0800

    conv+sgd

--- img/code/RNN/ttbp.py
+++ img/code/RNN/ttbp.py
@@ -1,6 +1,12 @@
-import graphviz as gz
+# import graphviz as gz
+#
+# g = gz.Digraph(filename='ttbp')
+# g.attr(rankdir='TB')
+#
+# g.view()
 
-g = gz.Digraph(filename='ttbp')
-g.attr(rankdir='TB')
 
-g.view()
+result1 = 67070 * 2.5 * 0.47 + 2.5 * 0.5 * 869000 * 0.47 * 0.47
+result2 = 1.8 * 84060 * 1.2 + 0.5 * 1.8 * 345000 * 1.2 * 1.2
+print(result1)
+print(result2)
